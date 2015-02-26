693fc37a06a96f79c47f73d1ed582e8a877b1b58
StephenWeiXu <stephenweixu@gmail.com>
2015-02-25 19:31:20 -0500

    add tweet text processing methods

--- Grab_data.py
+++ Grab_data.py
@@ -4,6 +4,8 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import csv
+import json
 import time
 
 consumer_key = 'gvUJ9I8YiQHaw2M0Fcfou2CC3'
@@ -15,13 +17,25 @@ class listener(StreamListener):
 
     def on_data(self, data):
     	try:
-    		tweet = data.split(',"text":"')[1].split('","source":"')[0]
+    		#tweet = data.split(',"text":"')[1].split('","source":"')[0]
+            raw_tweet = json.loads(data)
+            spec_tweet = {}
 
-    		saveThis = str(time.time()) + '::' + tweet
-	        saveFile = open('tweet_processed.txt', 'a')
-	        saveFile.write(saveThis)
-	        saveFile.write('\n')
-	        saveFile.close()
+            if raw_tweet.get('text', None) is None or raw_tweet.get('id_str', None) is None or str(raw_tweet.get('lang')) != 'en':
+                return True
+            raw_tweet['text'] = textProcessing(raw_tweet['text'])
+            car_label = get_label(raw_tweet['text'], car_brand_list)
+
+            if car_label != '':
+                for attribute in attributes:
+                    if raw_tweet.get(attribute, None) is not None:
+                        spec_tweet[attribute] = str(raw_tweet[attribute].encode('utf-8')).rstrip()
+                spec_tweet['brand'] = car_label
+
+                with open ('tweet_processed.csv', 'a') as f_tweet:
+                    writer = csv.DictWriter(f_tweet, fieldnames = attributes, delimiter = '|')
+                    writer.writerow(spec_tweet)
+
         	return True
         except BaseException, e:
         	print 'failed on_data, ', str(e)
@@ -30,8 +44,41 @@ class listener(StreamListener):
     def on_error(self, status):
         print status
 
-def carBrandList(:)
+# Process the tweet text, avoid irrelevant words being misassigned as car brand
+def textProcessing(tweetText):
+    tweetText_list = tweetText.lower().split()
+    for ind in range(0, len(tweetText_list)):
+        for word in tweetText_list:
+            if '@' in word or 'http' in word:
+                tweetText_list.remove(word)
+    tweetText_string = ' '.join(tweetText_list)
+
+    return tweetText_string
+
+def carBrandList():
+    f_carBrand = open('car_brand.txt')
+    car_brand = f_carBrand.read().lower().splitlines()
+    f_carBrand.close()
+    return car_brand
+
+def get_label(tweetText, carBrandList):
+    label = ''
+    tweetText_list = tweetText.lower().split()
+    for brand in carBrandList:
+        if brand in tweetText_list:
+            return brand
+    return label
+
+car_brand_list = carBrandList()
+extra_feature = ['drive', 'my', 'buy']
+total_list = car_brand_list + extra_feature 
+
+
+attributes = ['id_str', 'text', 'lang', 'brand']
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
+twitterStream.filter(track=total_list)
+
+
