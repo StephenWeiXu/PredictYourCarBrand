2c076e6a01af438c18207c4367f1f6b7ecbd260b
weiziliu <weizliu@umich.edu>
2015-03-14 00:20:48 -0400

    modify to get user_id

--- Grab_data.py
+++ Grab_data.py
@@ -1,5 +1,5 @@
 
-# Using Twitter API for grabing the tweets data from Twitter 
+# Using Twitter API for grabing the tweets data from Twitter
 
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -15,34 +15,45 @@ access_token_secret = 'T7hFnK0HGLuPLIC8Enxn4ecv2Ogf0cxDl4mqQ1BsDVSW3'
 
 class listener(StreamListener):
 
+    num_tweets = 0
+
     def on_data(self, data):
     	try:
     		#tweet = data.split(',"text":"')[1].split('","source":"')[0]
             raw_tweet = json.loads(data)
             spec_tweet = {}
 
-            if raw_tweet.get('text', None) is None or raw_tweet.get('id_str', None) is None or str(raw_tweet.get('lang')) != 'en':
+            if raw_tweet.get('text', None) is None or raw_tweet.get('user', None) is None or str(raw_tweet.get('lang')) != 'en':
                 return True
             raw_tweet['text'] = textProcessing(raw_tweet['text'])
             car_label = get_label(raw_tweet['text'], car_brand_list)
 
             if car_label != '':
+                listener.num_tweets += 1
+                print "%d tweets retrieved!" %(listener.num_tweets)
+                if listener.num_tweets > 5000: # Set the number of tweets to be retrieved
+                    print "\nTotal Number of tweets retrieved: ", listener.num_tweets
+                    return False
                 for attribute in attributes:
-                    if raw_tweet.get(attribute, None) is not None:
-                        spec_tweet[attribute] = str(raw_tweet[attribute].encode('utf-8')).rstrip()
+                    if 'user_id' == attribute:
+                        if raw_tweet.get('user', None) is not None:
+                            spec_tweet['user_id'] = str(raw_tweet['user']['id_str'].encode('utf-8')).rstrip()
+                    else:
+                        if raw_tweet.get(attribute, None) is not None:
+                            spec_tweet[attribute] = str(raw_tweet[attribute].encode('utf-8')).rstrip()
                 spec_tweet['brand'] = car_label
-
-                with open ('tweet_processed.csv', 'a') as f_tweet:
+                print spec_tweet
+                with open ('tweet_data_all.csv', 'a') as f_tweet:
                     writer = csv.DictWriter(f_tweet, fieldnames = attributes, delimiter = '|')
                     writer.writerow(spec_tweet)
 
         	return True
         except BaseException, e:
         	print 'failed on_data, ', str(e)
-        	time.sleep(5)
+        	time.sleep(1)
 
     def on_error(self, status):
-        print status
+       print status
 
 # Process the tweet text, avoid irrelevant words being misassigned as car brand
 def textProcessing(tweetText):
@@ -74,7 +85,7 @@ filter_list = carBrandList()
 for ind in range(0, len(car_brand_list)):
     filter_list[ind] = "my " + filter_list[ind]
 
-attributes = ['id_str', 'brand', 'text']
+attributes = ['user_id', 'brand', 'text']
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
